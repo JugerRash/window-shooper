362c7735725aef6decb5ef01e2f5bf35a4fe7e98
juger <juger@jugers-Mac.local>
2019-06-05 01:06:03 -0700

    first commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by juger on 6/5/19.
+//  Copyright © 2019 juger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
